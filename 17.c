27ae889bb59e6ee8904c01a5dedb934df12fcfac
Pegoku <pegoku08@gmail.com>
2025-09-30 09:25:07 +0200

    ex17

--- 17.c
+++ 17.c
@@ -5,11 +5,9 @@ int main() {
 
     printf("Introdueix dos nombres\n");
     scanf("%d %d", &num1, &num2);
-    // printf("Num1:%d\nNum2:%d\n", num1, num2);
     while (!(num1<num2)){
         printf("El primer ha de ser més petit que el segon\n");
         scanf("%d %d", &num1, &num2);
-        // printf("Num1:%d\nNum2:%d\n", num1, num2);
     }
     
     numDif = num2 - num1;
