5b0515c2ef75bdc6769f5ce946be555a9dfe2e83
Pegoku <pegoku08@gmail.com>
2025-10-02 14:02:29 +0200

    35

--- 34.c
+++ 34.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 
-int calcQuadrat(int num){
-    return num * num;
-}
-
 int main() {
-    printf("%d", calcQuadrat(4));
+    int num;
+    scanf("%d", &num);
+
+    switch (num)
+    {
+    case 1:
+    printf("Dilluns");
+        break;
+    case 2:
+    printf("Dimarts");
+        break;
+    case 3:
+    printf("Dimecres");
+        break;
+    case 4:
+    printf("Dijous");
+        break;
+    case 5:
+    printf("Divendres");
+        break;
+    case 6:
+    printf("Dissabte");
+        break;
+    case 7:
+    printf("Diumenge");
+        break;
+    default:
+        break;
+    }
+
     return 0;
 }
--- 35.c
+++ 35.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int calcQuadrat(int num){
-    return num * num;
+int compare (const void* a, const void* b){
+    return (*(int*)a-*(int*)b);
 }
 
+
 int main() {
-    printf("%d", calcQuadrat(4));
+    int num[3];
+
+    for (int i = 0; i < 3; i++){
+        scanf("%d", &num[i]);
+    }
+   qsort(num, 3, sizeof(int), compare);
+   for (int i = 0; i < 3; i++){
+    printf("%d ", num[i]);
+   }
+    
     return 0;
 }
