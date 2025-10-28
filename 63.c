17165f9de79725d88857418234c80b18cb678980
Pegoku <pegoku08@gmail.com>
2025-10-28 10:17:12 +0100

    63

--- 63.c
+++ 63.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int calcQuadrat(int num){
-    return num * num;
+void laterceralletra(char * c){
+    printf("La tercera lletra es: %c",c[2]);
 }
 
 int main() {
-    printf("%d", calcQuadrat(4));
+    char b[128];
+    printf("Introdueix una cadena: ");
+    scanf("%s", b);
+    laterceralletra(b);
     return 0;
 }
