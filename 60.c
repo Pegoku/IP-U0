7341e4cddff2d8b53817fe7c3d557eaa25862148
Pegoku <pegoku08@gmail.com>
2025-10-28 09:39:28 +0100

    60

--- 60.c
+++ 60.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int calcQuadrat(int num){
-    return num * num;
+int countChar(char * inStr, char l){
+    int i = 0;
+    for (int j = 0; j<strlen(inStr); j++) {
+        if (tolower(inStr[j]) == l) i++;
+    }
+    return i;
 }
 
 int main() {
-    printf("%d", calcQuadrat(4));
+    char s[128];
+    printf("Introdueix una cadena: ");
+    scanf("%s", s);
+    printf("A: %d\n", countChar(s,'a'));
+    printf("E: %d\n", countChar(s,'e'));
+    printf("I: %d\n", countChar(s,'i'));
+    printf("O: %d\n", countChar(s,'o'));
+    printf("U: %d\n", countChar(s,'u'));
     return 0;
 }
