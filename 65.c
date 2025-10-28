ab5d39002c785497404a93640fe3975dc445f2bd
Pegoku <pegoku08@gmail.com>
2025-10-28 10:48:53 +0100

    65

--- 65.c
+++ 65.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
 
-int calcQuadrat(int num){
-    return num * num;
+int checkIfIn(int * a, int n){
+    for (int j = 0; j<sizeof(a); j++){
+        if (a[j] == n) return 1;
+    }
+    return 0;
 }
 
 int main() {
-    printf("%d", calcQuadrat(4));
+    int nums[6];
+    srand(time(NULL));
+    for (int i = 0; i<6; i++){
+        int n;
+        do
+        {
+            n = (rand()%49)+1;
+            
+        } while (checkIfIn(nums, n)); 
+        nums[i] = n; 
+        printf("%d\n", n);
+    }
     return 0;
 }
