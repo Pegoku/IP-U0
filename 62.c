ea46f01dff60a53c5b334ad0238da7c6f4fa2345
Pegoku <pegoku08@gmail.com>
2025-10-28 10:11:18 +0100

    62

--- 62.c
+++ 62.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int calcQuadrat(int num){
-    return num * num;
-}
-
 int main() {
-    printf("%d", calcQuadrat(4));
+    int c;
+    double d;
+    printf("Conversor d'unitats. Tria una opció:\n\
+    (1) cm a polzada\n\
+    (2) polzada a cm\n");
+
+    scanf("%d", &c);
+    printf("Introdueix el nombre a convertir: ");
+    scanf("%lf", &d);
+    if (c==1){
+       printf("%lf in", d/2.54); 
+    }
+    if (c==2){
+       printf("%lf cm", d*2.54); 
+    }
     return 0;
+
 }
