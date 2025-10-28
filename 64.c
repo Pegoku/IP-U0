a740ecb4bdd3a6725485e145703f42b53aa27637
Pegoku <pegoku08@gmail.com>
2025-10-28 10:26:08 +0100

    64

--- 64.c
+++ 64.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int calcQuadrat(int num){
-    return num * num;
+void lalletrax(char * c, int num){
+    printf("La lletra n.%d es: %c", num+1, c[num]);
 }
 
 int main() {
-    printf("%d", calcQuadrat(4));
+    char b[128];
+    int d;
+    printf("Introdueix una cadena: ");
+    scanf("%s", b);
+    printf("Introdueix la posició de la lletra desitjada: ");
+    scanf("%d", &d);
+    lalletrax(b, d-1);
     return 0;
 }
