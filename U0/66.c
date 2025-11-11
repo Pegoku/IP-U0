f2aabc7d6a70266d4f2ba7a3a1ba96a4b877d68b
Pegoku <pegoku08@gmail.com>
2025-11-11 09:31:24 +0100

    66

--- U0/66.c
+++ U0/66.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
 
-int calcQuadrat(int num){
-    return num * num;
+int checkIfIn(int * a, int n){
+    for (int j = 0; j<sizeof(a); j++){
+        if (a[j] == n) return 1;
+    }
+    return 0;
+}
+
+char toQuiniela(int n){
+    switch (n)
+    {
+    case 0:
+        return 'X';
+        break;
+    case 1:
+        return '1';
+        break;
+    case 2:
+        return '2';
+        break;
+    
+    default:
+        break;
+    }
 }
 
 int main() {
-    printf("%d", calcQuadrat(4));
+    int nums[6];
+    srand(time(NULL));
+    for (int i = 0; i<14; i++){
+        int n;
+            n = (rand()%3);
+            
+        char r = toQuiniela(n); 
+        printf("%d: %c\n", i+1, r);
+    }
     return 0;
 }
